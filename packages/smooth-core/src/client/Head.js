c1edba8bf196b93f8531f3a9c9109b59d4426a64
Greg Bergé <greg@smooth-code.com>
2019-02-06 14:05:58 +0100

    feat(core): add default Head

---
+++ packages/smooth-core/src/client/Head.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+export default function Head() {
+  return (
+    <Helmet>
+      <title>Smooth.js | Code driven CMS</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Helmet>
+  )
+}
--- packages/smooth-core/src/client/Root.js
+++ packages/smooth-core/src/client/Root.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Routes from './Routes'
 import ErrorBoundary from './ErrorBoundary'
 import ErrorContext from './ErrorContext'
+import Head from './Head'
 
 export default function Root() {
   return (
     <ErrorContext.Consumer>
       {({ error }) => (
         <ErrorBoundary error={error}>
+          <Head />
           <Routes />
         </ErrorBoundary>
       )}
