d6ce8e9816b2c2c61665708f893fb488b93c7f29
Greg Bergé <greg@smooth-code.com>
2019-02-06 14:58:38 +0100

    fix(core): expose all router props

--- packages/smooth-core/src/router.js
+++ packages/smooth-core/src/router.js
@@ -5,6 +5,9 @@ export {
   Switch,
   Route,
   Redirect,
+  BrowserRouter,
+  HashRouter,
+  Router,
 } from 'react-router-dom'
 export { default as Link } from './client/Link'
 export { default as NavLink } from './client/NavLink'
