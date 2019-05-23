82e0ad80095829efa9139d068eedfebee1a1a943
Stefen Alper <stefen@smooth-code.com>
2019-05-23 10:52:28 +0200

    feat: track page titles

--- packages/smooth-plugin-google-analytics/src/smooth-browser.js
+++ packages/smooth-plugin-google-analytics/src/smooth-browser.js
@@ -1,10 +1,10 @@
 /* eslint-env browser */
 export function onRouteUpdate({ location }) {
   // Don't track while developing.
-  if (process.env.NODE_ENV === `production` && typeof ga === `function`) {
+  if (process.env.NODE_ENV === 'production' && typeof ga === 'function') {
     if (
       location &&
-      typeof window.excludeGAPaths !== `undefined` &&
+      typeof window.excludeGAPaths !== 'undefined' &&
       window.excludeGAPaths.some(rx => rx.test(location.pathname))
     ) {
       return
@@ -13,17 +13,16 @@ export function onRouteUpdate({ location }) {
     // wrap inside a timeout to make sure react-helmet is done with it's changes (https://github.com/gatsbyjs/gatsby/issues/9139)
     // reactHelmet is using requestAnimationFrame so we should use it too: https://github.com/nfl/react-helmet/blob/5.2.0/src/HelmetUtils.js#L296-L299
     const sendPageView = () => {
-      window.ga(
-        `set`,
-        `page`,
-        location
+      window.ga('set', {
+        page: location
           ? location.pathname + location.search + location.hash
           : undefined,
-      )
-      window.ga(`send`, `pageview`)
+        title: document.title,
+      })
+      window.ga('send', 'pageview')
     }
 
-    if (`requestAnimationFrame` in window) {
+    if ('requestAnimationFrame' in window) {
       requestAnimationFrame(() => {
         requestAnimationFrame(sendPageView)
       })
