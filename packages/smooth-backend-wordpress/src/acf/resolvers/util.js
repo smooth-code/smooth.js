272a595b6ca558e7ca00d1ab082ffe202b410302
Greg Bergé <greg@smooth-code.com>
2019-07-04 14:43:35 +0200

    fix: fix date, dateTime & time with i18n

--- packages/smooth-backend-wordpress/src/acf/resolvers/util.js
+++ packages/smooth-backend-wordpress/src/acf/resolvers/util.js
@@ -7,32 +7,7 @@ export function toRelativeUrl(baseUrl, url) {
   return finalUrl
 }
 
-function formatDateString(value) {
-  return value
-    .split('/')
-    .reverse()
-    .join('-')
-}
-
 export function formatDate(value) {
   if (typeof value !== 'string' || value === '') return null
-  return new Date(formatDateString(value))
-}
-
-export function formatDateTime(value) {
-  if (typeof value !== 'string' || value === '') return null
-  const [date, time, period] = value.split(' ')
-  return new Date(`${formatDateString(date)} ${time} ${period} UTC`)
-}
-
-export function formatTime(value) {
-  if (typeof value !== 'string' || value === '') return null
-  const [, hours, minutes, type] = value.match(/^(\d+):(\d+)\s+([ap]m)$/)
-  return `${[
-    type === 'pm' ? ((12 + Number(hours)) % 24).toString() : hours,
-    minutes,
-    '0',
-  ]
-    .map(x => x.padStart(2, '0'))
-    .join(':')}Z`
+  return new Date(value)
 }
