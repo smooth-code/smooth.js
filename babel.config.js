45de75a58c3651b99c532e3fcf98e631cfcafb4c
Greg Bergé <greg@smooth-code.com>
2019-08-30 15:26:50 +0200

    fix: fix dynamic import issue

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   presets: [
     '@babel/preset-react',
-    ['@babel/preset-env', { loose: true, targets: { node: 8 } }],
+    [
+      '@babel/preset-env',
+      { modules: false, loose: true, targets: { node: 8 } },
+    ],
   ],
   plugins: [
     '@loadable/babel-plugin',
+    '@babel/plugin-transform-modules-commonjs',
     '@babel/plugin-proposal-class-properties',
   ],
 }
